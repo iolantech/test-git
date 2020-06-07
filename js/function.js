05fadca521784b630183526e4fc63c625792328f
IolanTech <iolantech@gmail.com>
2020-06-07 14:33:08 -0300

    Adicionando event listener

--- js/function.js
+++ js/function.js
@@ -1,3 +1,6 @@
-function alerta(){
-    console.log("Testando novo branch!");
-}
+var h2 = document.querySelector('h2');
+
+h2.addEventListener('click', function() {
+    h2.innerHTML = 'Alterações no novo branch';
+})
+
